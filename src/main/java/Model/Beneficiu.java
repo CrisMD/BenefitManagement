627d4bc70b15c19c7c36ac2ced47069305f77663
razvan <razvan_fratean@yahoo.com>
2021-01-23 14:13:16 +0200

    CreateModel

---
+++ src/main/java/Model/Beneficiu.java
@@ -0,0 +1,37 @@
+package Model;
+
+public class Beneficiu {
+    String denumire;
+    String descriere;
+    Categorie categorie;
+
+    public String getDenumire() {
+        return denumire;
+    }
+
+    public void setDenumire(String denumire) {
+        this.denumire = denumire;
+    }
+
+    public String getDescriere() {
+        return descriere;
+    }
+
+    public void setDescriere(String descriere) {
+        this.descriere = descriere;
+    }
+
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    public Beneficiu(String denumire, String descriere, Categorie categorie) {
+        this.denumire = denumire;
+        this.descriere = descriere;
+        this.categorie = categorie;
+    }
+}
---
+++ src/main/java/Model/Categorie.java
@@ -0,0 +1,18 @@
+package Model;
+
+public class Categorie {
+    String denumire;
+
+    public String getDenumire() {
+        return denumire;
+    }
+
+    public void setDenumire(String denumire) {
+        this.denumire = denumire;
+    }
+
+    public Categorie(String denumire) {
+        this.denumire = denumire;
+    }
+}
+
